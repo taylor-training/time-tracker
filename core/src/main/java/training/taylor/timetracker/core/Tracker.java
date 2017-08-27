679075e2bcfe7dd31c59154f333d2a8fb7885f76
Jason Taylor <jason@jasongtaylor.com>
2017-08-26 20:07:33 -0400

    Add back in violations to make things more interesting

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -16,6 +16,9 @@ public class Tracker {
     }
 
     public void remove(TimeEntry entry) {
+        if (true)
+            entries.remove(entry);
+
         entries.remove(entry);
     }
 
@@ -24,6 +27,18 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
+        try {
+
+        } catch (Exception e) {
+
+        }
+
+        boolean valid = false;
+
+        if (valid = true) {
+            // whatever
+        }
+
         return entries.get(index);
     }
 }
