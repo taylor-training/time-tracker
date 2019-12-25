ec63d3d4168e0666f83b1e38416560a87774bd76
kat594 <58873458+kat594@users.noreply.github.com>
2019-12-25 11:51:55 +0530

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,7 +1,7 @@
 package training.taylor.timetracker.core;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+#import org.springframework.beans.factory.annotation.Autowired;
+#import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
